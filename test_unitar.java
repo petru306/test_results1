bf676e1ec07bb98c50891085b69583093299e035
petru306 <152027657+petru306@users.noreply.github.com>
2024-05-27 21:53:37 +0200

    Add files via upload

---
+++ test_unitar.java
@@ -0,0 +1,10 @@
+public class test_unitar {
+        public static void main(String[] args) {
+            
+         {
+        int result = 892 - 456 / 2 + 10;
+        System.out.println("Result: " + result);
+         }
+    }
+
+}
